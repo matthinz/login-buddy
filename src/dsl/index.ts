2fc0313ed0a05e7d6c4493c5a7308eb4e4fb2568
Matt Hinz <matt.hinz@gsa.gov>
2022-12-22 13:09:48 -0800

    Refactor dsl into subdir

--- src/dsl/index.ts
+++ src/dsl/index.ts
@@ -1,72 +1,7 @@
 import { launch, Browser, Page } from "puppeteer";
 import { promises as fs } from "fs";
 import path from "path";
-
-export type FlowRunOptions = {
-  baseURL?: string | URL;
-  browser?: Browser;
-  page?: Page;
-  skipNavigation?: boolean;
-};
-
-type FlowParams<State> = {
-  browser?: Browser;
-  page?: Page;
-  options?: FlowRunOptions;
-  state: State;
-};
-
-export type Flow<State> = {
-  /**
-   * Runs this flow and returns the generated state.
-   */
-  run(options?: FlowRunOptions): Promise<State>;
-
-  /**
-   * Evaluates a function, passing in a Puppeteer page and
-   * the current state.
-   * @param func Evaluation function. Can return a new state.
-   */
-  evaluate(func: (page: Page, state: State) => Promise<void>): Flow<State>;
-
-  /**
-   * Evaluates a function and allows modifying state.
-   * @param func
-   */
-  evaluateAndModifyState<NextState extends State>(
-    func: (page: Page, state: State) => Promise<NextState>
-  ): Flow<NextState>;
-
-  expectUrl(url: string | URL): Flow<State>;
-
-  generate<Key extends string, Value>(
-    key: Key,
-    generator: (state: State) => Value | Promise<Value>
-  ): Flow<State & { [K in Key]: Value }>;
-
-  passTo<OtherState extends State>(
-    otherFlow: Flow<OtherState>
-  ): Flow<State & OtherState>;
-
-  skipNavigation(): Flow<State>;
-
-  // Actions that can be taken
-
-  click(selector: string): Flow<State>;
-
-  submit(selector?: string): Flow<State>;
-
-  type(
-    selector: string | ((state: State) => string | Promise<string>),
-    text: string | ((state: State) => string | Promise<string>)
-  ): Flow<State>;
-
-  upload(
-    selector: string | ((state: State) => string | Promise<string>),
-    filename: string | ((state: State) => string | Promise<string>),
-    contents?: string | ((state: State) => string | Promise<string>)
-  ): Flow<State>;
-};
+import { Flow, FlowParams, FlowRunOptions } from "./types";
 
 /**
  * Starts a new flow, navigated to the given URL.
